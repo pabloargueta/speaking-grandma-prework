7b20fe063e9c3da7097d75af7070238d7f7d8abf
Pablo Argueta <pabloargueta27@gmail.com>
2018-05-23 00:29:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,15 @@
 
 def speak_to_grandma(phrase)
   
-  if (phrase != phrase.upcase)
-    return "HUH?! SPEAK UP, SONNY!"
+  if (phrase == "I LOVE YOU GRANDMA!")
+    "I LOVE YOU TOO PUMPKIN!"
     
+  elsif (phrase == phrase.upcase)
+    "NO, NOT SINCE 1938!"
+    
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  elsif (phrase == )  
   end
   
   
